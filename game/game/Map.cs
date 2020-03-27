ae543fef8434b36c73d88727265350789a35ea5d
sjackson01 <stevenpjackson0@gmail.com>
2020-03-27 13:41:47 -0500

    Add (map.cs>Hunk 1) : Create fields and constructor. Modify(program.cs>Hunk 1) : Update object instantiation to use constructor. Test read only field us with object instantiaton.

--- game/game/Map.cs
+++ game/game/Map.cs
@@ -6,5 +6,19 @@ namespace game
 {
     class Map
     {
+        // Readonly: values cannot be changed after object is constructed.
+        // We don't want our map size to change. 
+        public readonly int Width;  
+        public readonly int Height;  
+
+        // Create constructor 
+        // Intialize the objects fields with initial values. 
+        public Map(int width, int height)
+        {
+            // Use parameters to set fields.
+            Width = width;
+            Height = height; 
+
+        }
     }
 }
